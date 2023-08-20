f5e53e88b2d2108c080318e40235fa6402a4d667
haniadenm <haniadenm@gmail.com>
2023-08-20 17:28:51 -0400

    hw 11

---
+++ README.md
@@ -0,0 +1,30 @@
+# Express Note Taker
+
+[Link to the deployed application on Heruko](https://fierce-escarpment-71149.herokuapp.com/)
+
+## Description
+
+This is an express node application that allows the user to create and delete notes. The notes will be saved onto a local server (db.json file). 
+
+## Technology
+
+Note: the front-end was already developed for this project. My contribution was linking up the back-end and server This was completed using:
+
+* Node.js
+* Express
+* fs (read and write file)
+* HTML routes to already created HTML pages
+* API routes following CRUD basic functions (GET, POST, and DELETE)
+
+## User Story
+
+As a professional, I want a note-taking application that allows me to write, save, and delete notes so that I can keep my notes (and life) organized.
+
+## Instructions
+
+1. After connecting to the server through node or the deployed Heroku app and going to the index page (/), you will see a button to the note-taking page:
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependencies/server & middleware set up
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/Develop/public'));
+
+// Routes for index and notes pages
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+});
+
+// fs read file used to read/parse db.json file and put it into variable "notes"
+let notes
+
+const listOfNotes = fs.readFileSync('Develop/db/db.json');
+if (listOfNotes) {
+    notes = JSON.parse(listOfNotes);
+}
+
+// get request for api route (displays all notes)
+app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+});
+
+// post request
+app.post('/api/notes', function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    notes.push(newNote);
+    res.json(newNote);
+    renderNotes();
+})
+
+// delete request
+app.delete('/api/notes/:id', function (req, res) {
+    const NoteId = req.params.id;
+    //splice removed from array
+    notes.splice(NoteId, 1);
+    renderNotes();
+})
+
+//Function for assigning IDs to notes
+function assignNoteIds() {
+    for (i = 0; i < notes.length; i++) {
+        notes[i].id = i;
+    }
+}
+
+//Function for rendering notes using fs write file
+function renderNotes() {
+    fs.writeFileSync("Develop/db/db.json", JSON.stringify(notes, null, 2), function (err) {
+        if (err)
+            throw err
+    })
+    assignNoteIds();
+}
+
+// Starts the server
+app.listen(PORT, () => console.log("Server listening on: http://localhost:" + PORT))
